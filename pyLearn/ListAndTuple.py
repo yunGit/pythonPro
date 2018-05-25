4344ca74f464bd1689af75e8e6bc7a07c533d6bc
unknown <caoguoyun@cyou-inc.com>
2018-05-25 18:48:48 +0800

    List and Tuple

---
+++ pyLearn/ListAndTuple.py
@@ -0,0 +1,55 @@
+#
+# 有内置的一种数据类型是列表 list, 有序集合
+# 
+
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+l = len(classmates)
+print(l)
+
+print(classmates[0])
+# print(classmates[3])
+print(classmates[-1])
+
+# append()
+classmates.append('Adam')
+print(classmates)
+
+# pop()
+classmates.pop()
+print(classmates)
+classmates.pop(0)
+print(classmates)
+
+classmates[0] = "zhangsan"
+print(classmates)
+
+# list 元素类型可以不同
+L = ['Apple', 123, True]
+S = ['python', 'java', L, 'Scheme']
+print(S, len(S))
+
+
+
+# 另一种有序列表叫元组：tuple，一旦初始化不能改变
+classmatesT = ('Michael', 'Bob', 'Tracy')
+print(classmatesT)
+
+# classmatesT[0] = 'zhangsan' # 报错
+print(classmatesT)
+
+# 但是，若定义只有一个元素的tuple
+t = (1) # 此处定义的 t 非 tuple， 而是 1
+print(t)
+tt = (1,) # 此处定义的为 tuple
+print(tt)
+
+T = [
+	['Apple', 'Google','Microsoft'],
+	['Java', 'Python', 'Ruby', 'PHP'],
+	['Adam', 'Bart', 'Lisa']
+]
+
+print(T[0][0])
+print(T[1][1])
+print(T[2][2])
