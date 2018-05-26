2048074fd6ab47a0dbc4f3aa0e4ee59b2a03ba0a
unknown <caoguoyun@cyou-inc.com>
2018-05-26 10:14:58 +0800

    condition

---
+++ pyLearn/Condition.py
@@ -0,0 +1,46 @@
+#
+# 条件判断
+# 
+
+age = 20
+print("your age is ", age)
+if age >= 18:
+	print("adult")
+elif age >= 6:
+	print("teenager")
+else:
+	print("kid")
+
+if age:
+	print("age ", age)
+
+
+# input() - 读取用户输入
+birth = input('birth: ')
+# int() - 类型转换
+birth = int(birth)
+if birth < 2000:
+	print("00前")
+else:
+	print("00后")
+
+
+height = input("height(m) 	:")
+weight = input("weight(kg) 	:")
+
+height = float(height)
+weight = float(weight)
+
+bmi = weight / (height * height)
+print("your BMI is: %.2f" % bmi)
+
+if bmi < 18.5:
+	print("Too Slight")
+elif bmi < 25:
+	print("Normal")
+elif bmi <28:
+	print("Too Weight")
+elif bmi < 32:
+	print("Fat!")
+else:
+	print("Too Fat!!!")
